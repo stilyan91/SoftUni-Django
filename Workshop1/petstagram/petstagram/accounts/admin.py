70e2aafe154eae06702f0d978c890c062d30b130
stilyan91 <stilyan91@gmail.com>
2024-07-18 04:48:24 +0300

    add Users to admin panel

--- Workshop1/petstagram/petstagram/accounts/admin.py
+++ Workshop1/petstagram/petstagram/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+UserModel = get_user_model()
+
+@admin.register(UserModel)
+class PetstagramUserAdmin(admin.ModelAdmin):
+    pass
+
